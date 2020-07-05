aa894614507b401c3a79778ae6a29e0a06353e1b
Bryce Eldridge <bdeldri@gmail.com>
2020-07-05 13:41:54 -0600

    Removed website template, added java demo

---
+++ Intro_Java/hello.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class RTI2020Demo
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("Hello!");
+	}
+}
