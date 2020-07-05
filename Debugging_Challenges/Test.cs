9cbc673ecb155584a711ef7fc9e30d0eaf728f60
Bryce Eldridge <bdeldri@gmail.com>
2020-07-05 16:06:51 -0600

    Added test debugging challenge

---
+++ Debugging_Challenges/Test.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class DebugChallengeTest
+{
+	public static void Main()
+	{
+        int i = 0;
+        for(int j = 1; j <= 10; j++) {
+            Console.WriteLine(j * l);
+            i += 1;
+        }
+	}
+}
